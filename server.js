3f174d344af51ebe158e760d20ad910cca55b83c
ADoss <dabhinavgu@gmail.com>
2026-06-15 13:28:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const players = {};
 io.on("connection", (socket) => {
   console.log("Connected:", socket.id);
 
-  // REGISTER
   socket.on("register", ({ username, password }) => {
     if (!username || !password) {
       socket.emit("loginResult", {
@@ -41,6 +40,67 @@ io.on("connection", (socket) => {
     });
   });
 
-  // LOGIN
   socket.on("login", ({ username, password }) => {
-    if (!(users[username] &&
+    if (!(users[username] && users[username] === password)) {
+      socket.emit("loginResult", {
+        success: false,
+        msg: "Wrong username or password"
+      });
+      return;
+    }
+
+    if (activeUsers[username]) {
+      socket.emit("loginResult", {
+        success: false,
+        msg: "Account already logged in"
+      });
+      return;
+    }
+
+    activeUsers[username] = socket.id;
+
+    players[socket.id] = {
+      username,
+      x: 400,
+      y: 300,
+      health: 100
+    };
+
+    socket.emit("loginResult", {
+      success: true,
+      username
+    });
+
+    io.emit("playersUpdate", players);
+  });
+
+  socket.on("move", ({ x, y }) => {
+    if (!players[socket.id]) return;
+
+    players[socket.id].x = x;
+    players[socket.id].y = y;
+
+    socket.broadcast.emit("playerMoved", {
+      id: socket.id,
+      x,
+      y
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+
+    if (players[socket.id]) {
+      const username = players[socket.id].username;
+      delete activeUsers[username];
+    }
+
+    delete players[socket.id];
+
+    io.emit("removePlayer", socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});

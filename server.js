90fdf8f95472b2661ffb1416576688e7905f7920
ADoss <dabhinavgu@gmail.com>
2026-06-14 17:12:25 -0700

    Implement user login and registration
    Added user registration and login functionality with temporary storage.

--- server.js
+++ server.js
@@ -8,44 +8,61 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
+// SIMPLE LOGIN STORE (temporary memory)
+const users = {}; // username -> password
+const loggedIn = {}; // socket.id -> username
+
 const players = {};
 
 io.on("connection", (socket) => {
-  console.log("Player connected:", socket.id);
+  console.log("connected:", socket.id);
 
-  // create player
-  players[socket.id] = {
-    x: 400,
-    y: 300,
-    health: 100
-  };
+  // LOGIN
+  socket.on("register", ({ username, password }) => {
+    if (users[username]) {
+      socket.emit("loginResult", { success: false, msg: "User exists" });
+      return;
+    }
 
-  // send all players to new player
-  socket.emit("currentPlayers", players);
-
-  // broadcast new player
-  socket.broadcast.emit("newPlayer", {
-    id: socket.id,
-    player: players[socket.id]
+    users[username] = password;
+    socket.emit("loginResult", { success: true });
   });
 
-  // movement sync
-  socket.on("move", (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
+  socket.on("login", ({ username, password }) => {
+    if (users[username] === password) {
+      loggedIn[socket.id] = username;
 
-      socket.broadcast.emit("playerMoved", {
-        id: socket.id,
-        x: data.x,
-        y: data.y
-      });
+      players[socket.id] = {
+        x: 400,
+        y: 300,
+        username
+      };
+
+      socket.emit("loginResult", { success: true });
+      io.emit("playersUpdate", players);
+    } else {
+      socket.emit("loginResult", { success: false, msg: "Wrong login" });
     }
   });
 
-  // disconnect
+  // MOVEMENT
+  socket.on("move", (data) => {
+    if (!players[socket.id]) return;
+
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+
+    socket.broadcast.emit("playerMoved", {
+      id: socket.id,
+      x: data.x,
+      y: data.y
+    });
+  });
+
+  // DISCONNECT
   socket.on("disconnect", () => {
     delete players[socket.id];
+    delete loggedIn[socket.id];
     io.emit("removePlayer", socket.id);
   });
 });

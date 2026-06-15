0989609fea65bb159f223712beac426d20c20044
ADoss <dabhinavgu@gmail.com>
2026-06-15 15:20:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const players = {};
+
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  players[socket.id] = {
+    x: 400,
+    y: 300,
+    size: 30
+  };
+
+  io.emit("players", players);
+
+  socket.on("move", (data) => {
+    if (!players[socket.id]) return;
+
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+
+    socket.broadcast.emit("playerMove", {
+      id: socket.id,
+      x: data.x,
+      y: data.y
+    });
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("removePlayer", socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
